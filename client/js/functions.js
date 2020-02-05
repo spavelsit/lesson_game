551e8976b4c3854e712cb9e62e2c26f143b278aa
thejokermsk <thejoker.msk@icloud.com>
2020-02-05 16:31:25 +0300

    Done: Connect server part

--- client/js/functions.js
+++ client/js/functions.js
@@ -8,3 +8,16 @@ function isCollision(rect1 = {}, rect2 = {}) {
     rect1.y < rect2.y + rect2.height && 
     rect1.y + rect1.height > rect2.y
 }
+
+async function createScore(name, time, score) {
+  return await fetch('http://localhost:3000/api/score/create', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json;charset=utf-8'},
+    body: JSON.stringify({name, time, score})
+  })
+}
+
+async function getScore() {
+  return await fetch('http://localhost:3000/api/score')
+    .then(res => res.json())
+}
